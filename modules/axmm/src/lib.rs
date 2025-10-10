3b81289f520e33a75b489963649f0d6c2c46a9a9
周睿 <zrufo747@outlook.com>
2025-10-10 16:20:47 +0800

    fix: iomap

--- modules/axmm/src/lib.rs
+++ modules/axmm/src/lib.rs
@@ -123,7 +123,13 @@ pub fn iomap(addr: PhysAddr, size: usize) -> AxResult<VirtAddr> {
 
     let flags = MappingFlags::DEVICE | MappingFlags::READ | MappingFlags::WRITE;
     let mut tb = kernel_aspace().lock();
-    tb.map_linear(virt_aligned, addr_aligned, size_aligned, flags)?;
+    match tb.map_linear(virt_aligned, addr_aligned, size_aligned, flags) {
+        Err(AxError::AlreadyExists) => {}
+        Err(e) => {
+            return Err(e);
+        }
+        Ok(_) => {}
+    }
     // flush TLB
     tb.protect(virt_aligned, size_aligned, flags)?;
     Ok(virt)
